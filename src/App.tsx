a6055c159e960fdc80c8711960d33db0eb159a1c
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2021-12-13 16:38:23 +0900

    ~5.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -69,6 +70,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
---
+++ src/ToDoList.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function ToDoList() {
+  const [toDo, setToDo] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(toDo);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder="write a todo" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+export default ToDoList;
